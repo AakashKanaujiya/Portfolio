5ed2f4100b30b7a922318b72db64c54dfbbb9035
Aakash Kanaujiya <aakasharya1430@gmail.com>
2024-08-09 02:18:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var typed = new Typed('#element', {
+    strings: ['Web Designer', 'Web Developer','UI Designer'] ,
+    typeSpeed: 50,
+    backSpeed:50,
+    loop: true,
+});
+
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname){
@@ -41,3 +49,20 @@ document.querySelector("#portfolio").addEventListener("click",()=>{
 document.querySelector("#contact").addEventListener("click",()=>{
     sidemeu.style.right = "-220px";
 })
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx1z_fJ3BBmgdW8P6zyRKeP2H9us9Uk3aLsJbdtUjsurJBbUz6iS5_IuEoZP4TwDo3xHA/exec'
+        const form = document.forms['submit-to-google-sheet']
+        const msg = document.getElementById("msg");
+
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML = "Message sent successfully !";
+                setTimeout(function(){
+                    msg.innerHTML = ""
+                },3000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+        })
